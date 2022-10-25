087353fb32b0379246400941870523558e28063f
ZakariaNu <s362082@oslomet.no>
2022-10-26 01:19:23 +0200

    fjernet main-metode

--- src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
@@ -244,18 +244,5 @@ public class SBinTre<T> {
         throw new UnsupportedOperationException("Ikke kodet ennå!");
     }
 
-    public static void main(String[] args) {
-        Integer[] a = {4, 7, 2, 9, 4, 10, 8, 7, 4, 6};
-        SBinTre<Integer> tre = new SBinTre<>(Comparator.naturalOrder());
-        for (int verdi : a) {
-            tre.leggInn(verdi);
-        }
-
-        System.out.println(tre.antall());      // Utskrift: 10
-        System.out.println(tre.antall(5));     // Utskrift: 0
-        System.out.println(tre.antall(4));     // Utskrift: 3
-        System.out.println(tre.antall(7));     // Utskrift: 2
-        System.out.println(tre.antall(10));    // Utskrift: 1
-    }
 
 } // ObligSBinTre
